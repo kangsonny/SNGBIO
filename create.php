cb69211ab7b9fa38ce32cec682684d21ccc67890
kangsonny <96951885+kangsonny@users.noreply.github.com>
2022-02-18 16:21:07 +0900

    Add files via upload

---
+++ create.php
@@ -0,0 +1,22 @@
+<?php
+require('lib/print.php');
+require('view/top.php');
+  ?>
+    <a href="create.php">create</a>
+    <form action="create_process.php" method="post">
+      <p>
+        <input type="text" name="title" placeholder="Title">
+      </p>
+      <p>
+        <textarea name="description" rows="15" cols="80"
+        placeholder="Description"></textarea>
+      </p>
+      <p>
+          <input type="submit">
+      </p>
+
+    </form>
+
+<?php
+require('view/bottom.php');
+ ?>
